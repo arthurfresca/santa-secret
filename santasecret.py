527f09cb687320c397b507de3e1cb457f34d3fbf
Arthur Fresca <arthur.m.fresca@gmail.com>
2024-12-07 16:28:54 -0500

    Fixing encriptation

--- santasecret.py
+++ santasecret.py
@@ -1,10 +1,26 @@
 from flask import Flask, request, jsonify
 from cryptography.fernet import Fernet
+import base64
+from Crypto.Cipher import AES
 import random
+import base64
+import json
+
+def encrypt(text, key):
+    cipher = AES.new(key.encode('utf-8'), AES.MODE_ECB)
+    text_padded = text + (16 - len(text) % 16) * chr(16 - len(text) % 16)
+    encrypted = cipher.encrypt(text_padded.encode('utf-8'))
+    return base64.b64encode(encrypted).decode('utf-8')
+
+def decrypt(encrypted, key):
+    cipher = AES.new(key.encode('utf-8'), AES.MODE_ECB)
+    encrypted_bytes = base64.b64decode(encrypted)
+    decrypted_padded = cipher.decrypt(encrypted_bytes).decode('utf-8')
+    return decrypted_padded[:-ord(decrypted_padded[-1])]
+
+key = "mysecretkey12345"  # Use uma chave de 16 caracteres
 
 app = Flask(__name__)
-
-cipher = Fernet("wgk06QLqaTDl89NkOCqE44iibdJF19R-TkTsNFFZxSY=")
 
 @app.route('/generate-secret-santa', methods=['POST'])
 def generate_secret_santa():
@@ -27,7 +43,7 @@ def generate_secret_santa():
     base_url = "https://arthurfresca.github.io/santa-secret/#"
     urls = []
     for giver, receiver in pairs:
-        encrypted_receiver = cipher.encrypt(receiver.encode()).decode()
+        encrypted_receiver = encrypt(receiver, key)
         urls.append(f"{base_url}{giver}/{encrypted_receiver}")
 
     return jsonify({'urls': urls})
